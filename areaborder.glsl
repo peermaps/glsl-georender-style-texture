8e4c53d1b9b643c517d1dc57e5ff9893779d7ec0
marina <marina.kukso@gmail.com>
2022-06-04 21:07:02 -1000

    .glsl files now use formula to convert from pixels to uv values

--- areaborder.glsl
+++ areaborder.glsl
@@ -7,26 +7,21 @@ float lineHeight = 8.0*zoomCount;
 float areaHeight = 6.0*zoomCount;
 float areaBorderHeight = 3.0*zoomCount;
 float totalHeight = pointHeight + lineHeight + areaHeight + areaBorderHeight;
-
-vec2 texRange = vec2((pointHeight + lineHeight + areaHeight)/totalHeight,(pointHeight + lineHeight + areaHeight + areaBorderHeight)/totalHeight);
+float areaBorderStart = pointHeight + lineHeight + areaHeight;
 
 AreaBorder readAreaBorder(sampler2D styleTexture, float featureType, float zoom, float featureCount) {
   float n = 3.0;
+  float px = featureType; //pixel x
+  float py = areaBorderStart + n * (zoomStart + floor(zoom)); //pixel y
+
   vec4 d0 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (0.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * vec4(1,1,1,2.55);
+    px/featureCount+0.5/featureCount, (py+0.0)/totalHeight + 0.5/totalHeight)) * vec4(1,1,1,2.55);
+
   vec4 d1 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (1.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * 255.0;
+    px/featureCount+0.5/featureCount, (py+1.0)/totalHeight + 0.5/totalHeight)) * 255.0;
+
   vec4 d2 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (2.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * 255.0;
+    px/featureCount+0.5/featureCount, (py+2.0)/totalHeight + 0.5/totalHeight)) * 255.0;
 
   AreaBorder areaBorder;
   areaBorder.color = d0;
--- line.glsl
+++ line.glsl
@@ -7,58 +7,36 @@ float lineHeight = 8.0*zoomCount;
 float areaHeight = 6.0*zoomCount;
 float areaBorderHeight = 3.0*zoomCount;
 float totalHeight = pointHeight + lineHeight + areaHeight + areaBorderHeight;
-
-vec2 texRange = vec2(pointHeight/totalHeight, (pointHeight + lineHeight)/totalHeight);
+float lineStart = pointHeight;
 
 Line readLine(sampler2D styleTexture, float featureType, float zoom, float featureCount) {
   float n = 8.0;
+  float px = featureType; //pixel x
+  float py = lineStart + n * (zoomStart + floor(zoom)); //pixel y
+
   vec4 d0 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (0.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * vec4(1,1,1,2.55);
+    px/featureCount+0.5/featureCount, (py+0.0)/totalHeight + 0.5/totalHeight)) * vec4(1,1,1,2.55);
 
   vec4 d1 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (1.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * vec4(1,1,1,2.55);
+    px/featureCount+0.5/featureCount, (py+1.0)/totalHeight + 0.5/totalHeight)) * vec4(1,1,1,2.55);
 
   vec4 d2 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (2.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * 255.0;
+    px/featureCount+0.5/featureCount, (py+2.0)/totalHeight + 0.5/totalHeight)) * 255.0;
 
   vec4 d3 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (3.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * 255.0;
+    px/featureCount+0.5/featureCount, (py+3.0)/totalHeight + 0.5/totalHeight)) * 255.0;
 
   vec4 d4 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (4.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * vec4(1,1,1,2.55);
+    px/featureCount+0.5/featureCount, (py+4.0)/totalHeight + 0.5/totalHeight)) * vec4(1,1,1,2.55);
 
   vec4 d5 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (5.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * vec4(1,1,1,2.55);
+    px/featureCount+0.5/featureCount, (py+5.0)/totalHeight + 0.5/totalHeight)) * vec4(1,1,1,2.55);
 
   vec4 d6 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (6.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * 255.0;
+    px/featureCount+0.5/featureCount, (py+6.0)/totalHeight + 0.5/totalHeight)) * 255.0;
 
   vec4 d7 = texture2D(styleTexture, vec2(
-    featureType/featureCount+0.5/featureCount,
-    ((floor(zoom)-zoomStart)/zoomCount + (7.0*2.0+1.0)/(n*zoomCount*2.0))
-      * (texRange.y-texRange.x) + texRange.x
-  )) * 255.0;
+    px/featureCount+0.5/featureCount, (py+7.0)/totalHeight + 0.5/totalHeight)) * 255.0;
 
 
   Line line;
