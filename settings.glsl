38becf198d9a003f25c1e78d3ad322f362b82dbb
marina <marina.kukso@gmail.com>
2022-09-27 12:21:08 -1000

    fixed spritemeta height in settings; small change in spritemeta.glsl

--- settings.glsl
+++ settings.glsl
@@ -2,14 +2,14 @@
 
 Settings defaultSettings(float spriteCount) {
   Settings settings;
+  settings.featureCount = 1240.0;
   settings.zoomStart = 1.0;
   settings.zoomCount = 21.0;
   settings.pointHeight = 7.0*settings.zoomCount;
   settings.lineHeight = 8.0*settings.zoomCount;
   settings.areaHeight = 6.0*settings.zoomCount;
   settings.areaBorderHeight = 3.0*settings.zoomCount; 
-  settings.spriteMetaHeight = 2.0*spriteCount;
-  settings.featureCount = 1240.0;
+  settings.spriteMetaHeight = 2.0*(spriteCount/settings.featureCount)+1.0;
   return settings;
 }
 
--- spritemeta.glsl
+++ spritemeta.glsl
@@ -12,18 +12,16 @@ vec2 spriteLocation(Settings settings, float spriteIndex) {
 SpriteMeta readSpriteMeta(sampler2D styleTexture, vec2 imageSize, float index) {
   Settings settings = defaultSettings(7.0);
 
-  //vec2 px0 = spriteLocation(settings, index);
   vec2 px0 = spriteLocation(settings, index);
   vec2 px1 = px0 + vec2(0,1);
   vec2 uv0 = px0/(imageSize - 1.0);
   vec2 uv1 = px1/(imageSize - 1.0);
 
-  vec4 d0 = texture2D(styleTexture, uv0)*256.0;
+  vec4 d0 = texture2D(styleTexture, uv0)*255.0;
   vec4 d1 = texture2D(styleTexture, uv1)*255.0;
 
   SpriteMeta spriteMeta;
   spriteMeta.width = d0.x*256.0 + d0.y;
-  //spriteMeta.height = mod(d0.z, 128.0)*256.0 + d0.w;
   spriteMeta.height = mod(d0.z, 128.0)*256.0 + d0.w;
   spriteMeta.type = floor(d0.z/128.0);
   spriteMeta.x = d1.x*256.0 + d1.y;
