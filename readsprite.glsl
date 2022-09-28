b12212056e85e000a21568c0e8eea56bca0dd3ad
marina <marina.kukso@gmail.com>
2022-09-28 12:40:39 -1000

    readsprite.glsl: fixed sprite height calculation

--- readsprite.glsl
+++ readsprite.glsl
@@ -6,17 +6,11 @@ vec4 readSprite(sampler2D styleTexture, vec2 imageSize, SpriteMeta spriteMeta, v
   Settings settings = defaultSettings(7.0);
   float spriteStart = settings.pointHeight + settings.lineHeight + settings.areaHeight + settings.areaBorderHeight + settings.spriteMetaHeight;
 
-  //float x = 0.0;
   float x = spriteMeta.x;
-  //float y = 504.0+2.0;
   float y = spriteMeta.y + spriteStart;
-  //float width = 499.0 - x;
   float width = spriteMeta.width;
-  //float height = 747.0 - y;
-  float height = spriteMeta.height;
-  //float iwidth = 1240.0;
+  float height = spriteMeta.height-1.0;
   float iwidth = imageSize.x;
-  //float iheight = 1007.0;
   float iheight = imageSize.y;
   vec2 tuv = vec2(
     (uv.x*width+x)/(iwidth-1.0),
